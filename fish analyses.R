3b0ee9f432936b17b7bdfbf634afa26ef42b0b74
rosehartman <rosehartman@gmail.com>
2019-12-04 11:19:40 -0800

    additional fish analyses

--- fish analyses.R
+++ fish analyses.R
@@ -16,8 +16,34 @@ fishcatch = mutate(fishcatch, sample = paste(StationCode, MethodCode, SampleDate
 ggplot(fishcatch, aes(x = Year)) + geom_bar()+
   facet_wrap(~StationCode, scales = "free_y")
 
-#table of samples per year
-samps = as.data.frame(table(fishcatch$StationCode, fishcatch$Year))
-names(samps) = c("StationCode", "Year", "count")
+#table of samples per station per year
+samps = group_by(fishcatch, StationCode, Year, MethodCode) %>%
+  summarize(ntrawl = length(unique(sample)))
 
-fishRecent = filter(fishcatch, Year > 1995)
+#filter it to the past 20 years
+fishRecent = filter(fishcatch, Year > 1997)
+sampsRecent = filter(samps, Year >1997)
+
+#Check out which stations are sampled continuously
+stas = group_by(sampsRecent, StationCode, MethodCode) %>%
+  summarize(trawls = sum(ntrawl))
+
+#WE SHOULD probably take out all the beach seines, midwtaer trawls,
+#and all the stations that are sampled less than 20 times in the past 20 years
+
+fishR2 = filter(fishRecent, MethodCode == "OTR", StationCode %in% 
+                  stas$StationCode[which(stas$trawls>20)])
+
+#what stations are left?
+unique(fishR2$StationCode)
+
+#how does the sampling balance across months?
+fishR2$Month = month(fishR2$SampleDate)
+
+
+#it looks like we have roughly even distribution of samples across the year
+stas2 = group_by(fishR2, StationCode, Month) %>%
+  summarize(trawls = length(unique(sample)))
+ggplot(stas2, aes(x= Month, y = trawls)) + geom_bar(stat = "identity") +
+  facet_wrap(~StationCode)
+
